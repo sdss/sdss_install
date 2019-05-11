fab833dcf6afcd5d8711067f08a0985d301746c6
N Benjamin Murphy <n.benjamin.murphy@astro.utah.edu>
2019-05-10 18:36:14 -0600

    Removed GraphQL related code

--- python/sdss_install/install5/__init__.py
+++ python/sdss_install/install5/__init__.py
@@ -10,11 +10,6 @@ This package contains code for installing SDSS-V software products.
 from __future__ import absolute_import, division, print_function, unicode_literals
 # The line above will help with 2to3 support.
 
-from .Repositories import Repositories
-from .Branches import Branches
-from .Tags import Tags
 from .Install5 import Install5
-#from .most_recent_tag import most_recent_tag
-#from .version import version
 
 
