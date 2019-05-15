6ae910ea2069dafb63f2054f810f4d47d7418a6d
Joel Brownstein <joelbrownstein@sdss.org>
2019-05-15 14:55:48 -0600

    add lua

--- python/sdss_install/utils/module.py
+++ python/sdss_install/utils/module.py
@@ -35,7 +35,8 @@ class Module:
 
     def set_modules(self):
         self.set_modules_home()
-        self.set_tclsh()
+        self.set_modules_lang()
+        if self.modules_lang['tcl']: self.set_tclsh()
         self.set_ready()
 
     def set_modules_home(self):
@@ -47,11 +48,16 @@ class Module:
                 self.modules_home = {'dir': modules_home}
             except: self.modules_home = dict()
             if self.modules_home:
-                self.modules_home['executable'] = join(modules_home, "modulecmd.tcl")
+                self.modules_home['lua'] = join(modules_home, "init", "lmodrc.lua")
+                self.modules_home['tcl'] = join(modules_home, "modulecmd.tcl")
             else:
                 self.ready = False
                 self.logger.error('Unable to set_modules_home. ' +
                                   'self.modules_home: {}.'.format(self.modules_home))
+    def set_modules_lang(self):
+        self.modules_lang = {}
+        self.modules_lang['lua'] = self.modules_home['lua'] and exists(self.modules_home['lua'])
+        self.modules_lang['tcl'] = self.modules_home['tcl'] and exists(self.modules_home['tcl'])
 
     def set_tclsh(self):
         '''Set tclsh exec directory.'''
@@ -70,40 +76,38 @@ class Module:
         '''Set self.ready after setting modules information.'''
         self.ready = (self.logger and 
                       self.modules_home and
-                      self.tclsh and exists(self.tclsh) and
                       self.modules_home['dir'] and exists(self.modules_home['dir']) and
-                      self.modules_home['executable'] and exists(self.modules_home['executable']))
+                      (self.modules_lang['lua'] or
+                      (self.tclsh and exists(self.tclsh) and self.modules_lang['tcl'])))
 
     def set_command(self, command=None, arguments=None):
         self.command = list()
         if self.ready:
             if command:
-                self.command = [self.tclsh, self.modules_home['executable'], 'python', command]
-                if arguments:
+                self.command = ['module', command] if self.modules_lang['lua'] else [self.tclsh, self.modules_home['tcl'], 'python', command] if self.modules_lang['tcl'] else None
+                if self.command and arguments:
                     self.command.append(string.join(arguments))
             else:
                 self.ready = False
                 self.logger.error('Unable to set_command. ' +
                                   'command: {}.'.format(command))
 
-    def old_crappy_method(self):
-        self.set_command("-V")
-        proc = Popen(self.command)
 
     def set_version(self):
         '''Set the Modules Release Tcl version string returned by modules --version.'''
         self.version = str()
         if self.ready:
-            self.set_command("-V")
-            proc = Popen(self.command, stdout=PIPE, stderr=PIPE)
-            if proc:
-                (stdout, stderr) = proc.communicate() if proc else (None,None)
-                self.returncode = proc.returncode if proc else None
-                self.version = stderr.strip() if self.returncode == 0 and stderr else None
-            else:
-                self.ready = False
-                self.logger.error('Unable to set_version. ' +
-                                  'proc: {}.'.format(proc))
+            self.set_command("--version")
+            if self.command:
+                proc = Popen(self.command, stdout=PIPE, stderr=PIPE)
+                if proc:
+                    (stdout, stderr) = proc.communicate() if proc else (None,None)
+                    self.returncode = proc.returncode if proc else None
+                    self.version = stderr.strip() if self.returncode == 0 and stderr else None
+                else:
+                    self.ready = False
+                    self.logger.error('Unable to set_version. ' +
+                                      'proc: {}.'.format(proc))
 
     def set_major_minor_patch(self):
         '''From the the Modules Release Tcl version string, set version major, minor, and patch.'''
