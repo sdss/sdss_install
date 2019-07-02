4522f967c322bae2e9b9456313a4b2ab88e916b3
N Benjamin Murphy <n.benjamin.murphy@astro.utah.edu>
2019-07-02 17:08:26 -0600

    Further fix of Python3 incompatability in Install5.get_matches

--- python/sdss_install/install5/Install5.py
+++ python/sdss_install/install5/Install5.py
@@ -173,7 +173,7 @@ class Install5:
         matches = None
         if self.ready:
             if regex and string:
-                string = str(string)
+                string = str(string.decode("utf-8"))
                 pattern = compile(regex)
                 iterator = pattern.finditer(string)
                 matches = list()
