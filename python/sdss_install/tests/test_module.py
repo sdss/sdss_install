0fb661c3e39cc5fce137a00d561af49f10d5b935
Brian Cherinka <havok2063@hotmail.com>
2020-02-28 14:00:46 -0500

    trying to skip module test for travis

--- python/sdss_install/tests/test_module.py
+++ python/sdss_install/tests/test_module.py
@@ -7,13 +7,14 @@
 # Created: Tuesday, 25th February 2020 9:28:37 am
 # License: BSD 3-clause "New" or "Revised" License
 # Copyright (c) 2020 Brian Cherinka
-# Last Modified: Tuesday, 25th February 2020 11:52:31 am
+# Last Modified: Friday, 28th February 2020 2:00:03 pm
 # Modified By: Brian Cherinka
 
 
 from __future__ import print_function, division, absolute_import
 
 import pytest
+import os
 from sdss_install.utils.module import Module
 
 
@@ -29,6 +30,7 @@ def test_module_ready(module):
     assert module.ready is True
 
 
+@pytest.mark.skipif(os.getenv("TRAVIS") is not None, reason='skipping if on travis')
 @pytest.mark.parametrize('install', [('-v', '-t', '--github')], ids=['options'], indirect=True)
 def test_module_list(module):
     loaded_modules = module.list_modules()
